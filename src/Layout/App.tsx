a57df2e761e727e9c4ad129eee9a00fea239999b
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-08-19 10:53:57 -0600

    Add Flowbite

--- src/Layout/App.tsx
+++ src/Layout/App.tsx
@@ -1,11 +1,12 @@
 import '../Styles/App.css'
+import { Button } from "flowbite-react";
 
 function App() {
-
 
   return (
     <>
       <h1 className="text-cyan-600 text-3xl font-bold underline" >Hello World.</h1>
+      <Button>Click me</Button>
     </>
   )
 }
